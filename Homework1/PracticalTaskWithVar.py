61f56619e48cfe31a227bb56f3656d982d1ca180
Viktoriia <46558739+ViktoriiaSpiridonova@users.noreply.github.com>
2019-01-11 22:17:20 +0200

    Practical Task for Homework1
    Task with var_int etc. finished

---
+++ Homework1/PracticalTaskWithVar.py
@@ -0,0 +1,10 @@
+var_int = 10
+var_float = 8.4
+var_str = "No"
+big_int = var_int*3.5
+print(big_int)
+print(var_float-1)
+print(var_int/var_float)
+print(big_int/var_float)
+var_str = (var_str*2 + ("Yes")*3)
+print(var_str)
