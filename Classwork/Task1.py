4875c394cf45a9a069fe2e45e15a4bd46d4a665a
Viktoriia <46558739+ViktoriiaSpiridonova@users.noreply.github.com>
2019-01-17 14:07:34 +0200

    Update Task1.py

--- Classwork/Task1.py
+++ Classwork/Task1.py
@@ -3,7 +3,7 @@ b = int(input("Input number b ="))
 if a >= b:
     print("{} is bigger".format(a))
 else:
-    print({} is bigger".format(b))
+    print("{} is bigger".format(b))
     
 a = float(input("Input number a ="))
 if a%2 == 0:
