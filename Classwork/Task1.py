06ae356d222be8169ecebbc6ee878103c3cb72fa
Viktoriia <46558739+ViktoriiaSpiridonova@users.noreply.github.com>
2019-01-17 14:06:18 +0200

    Bigger and Even/Odd number
    Task for classroom completed

---
+++ Classwork/Task1.py
@@ -0,0 +1,13 @@
+a = int(input("Input number a ="))
+b = int(input("Input number b ="))
+if a >= b:
+    print("{} is bigger".format(a))
+else:
+    print({} is bigger".format(b))
+    
+a = float(input("Input number a ="))
+if a%2 == 0:
+    print("{} is even".format(a))
+else:
+    print("{} is odd".format(a))
+
