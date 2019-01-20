728a0fcbdf55dee4d813255f69b5102c39572db9
Viktoriia <46558739+ViktoriiaSpiridonova@users.noreply.github.com>
2019-01-20 16:14:11 +0200

    Update Task1.py

--- Classwork/Task1.py
+++ Classwork/Task1.py
@@ -1,13 +1,18 @@
+# 1.  Написати скрипт, який з двох введених чисел визначить, яке з них більше, а яке менше.
+
+
 a = int(input("Input number a ="))
 b = int(input("Input number b ="))
 if a >= b:
     print("{} is bigger".format(a))
 else:
     print("{} is bigger".format(b))
-    
+ 
+
+# 2.  Написати скрипт, який перевірить чи введене число парне чи непарне і вивести відповідне повідомлення.
+
 a = float(input("Input number a ="))
 if a%2 == 0:
     print("{} is even".format(a))
 else:
     print("{} is odd".format(a))
-
