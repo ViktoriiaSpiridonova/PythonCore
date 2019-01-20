15eebe93af8c506ce5aeded0a838cdbd18728171
Viktoriia <46558739+ViktoriiaSpiridonova@users.noreply.github.com>
2019-01-20 16:06:06 +0200

    Update Task2.py

--- Homework 2/Task2.py
+++ Homework 2/Task2.py
@@ -1,5 +1,14 @@
-num = 3456
+# Задано чотирицифрове натуральне число. 
+# Знайти добуток цифр цього числа.
+# Записати число в реверсному порядку.
+# Посортувати цифри, що входять в дане число
+
+num = str(input("Enter 4 digit number:"))
+multiplic = 1
+for i in num:
+  multiplic *= int(i)
+print("Multiplication is " + str(multiplic))
 c = str(num)
-print(c[::-1])
+print("Number in reverse is ", c[::-1])
 s = str(num)
-print(sorted(s))
+print("Sorted number is ", sorted(s))
