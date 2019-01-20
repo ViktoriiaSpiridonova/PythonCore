284527cb3dac4e3aad5a540821a278d61a3ee8e6
Viktoriia <46558739+ViktoriiaSpiridonova@users.noreply.github.com>
2019-01-20 16:23:13 +0200

    Create Task3-3.py

---
+++ Classwork/Task3-3.py
@@ -0,0 +1,18 @@
+# 3.  Перевірити чи список містить непарні числа. (Підказка: використати оператор break)
+          
+list_number = [2,4,6,8,10]
+contain_odd = False
+for item in list_number:
+    if not item % 2 == 0:
+        contain_odd = True
+        break
+if contain_odd:
+    print('There are odd numbers in list')
+else:
+    print('There are only even numbers in list')
+
+
+list = [2, 1, 4, 6, 1, 4, 8, 6]
+for i in range(len(list)):
+    list[i] = float(list[i])
+print(list) 
