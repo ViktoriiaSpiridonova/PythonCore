b6ecd8859276b1b517ff1e39186b0fb00c3fbec8
Viktoriia <46558739+ViktoriiaSpiridonova@users.noreply.github.com>
2019-01-20 15:51:50 +0200

    Update Task1.py

--- Homework1/Task1.py
+++ Homework1/Task1.py
@@ -1,6 +1,16 @@
-n = input("What is your name?")
-print("Hello, " +n +"!")
-a = input("How old are you?")
-print("Your age is " +a)
-l = input("Where do you live?")
-print("You live in "+l)
+# Створіть скрипт, який би запитував у користувача:
+# Його ім'я: "What is your name?"
+# Вік: "How old are you?"
+# Місце проживання: "Where do you live?"
+# А потім виводив наступні рядки:
+# “Hello, ім'я!"
+# “Your age is вік"
+# “You live in місце проживання"
+
+
+name = input("What is your name?")
+age = input("How old are you?")
+live = input("Where do you live?")
+print("Hello, " +name +"!")
+print("Your age is " +age)
+print("You live in "+live)
