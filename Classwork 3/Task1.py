d9669392e115c5f7246d9102cfb1bc7ff336224f
Viktoriia <46558739+ViktoriiaSpiridonova@users.noreply.github.com>
2019-01-22 17:05:12 +0200

    Max number from list
    Task 1 completed

---
+++ Classwork 3/Task1.py
@@ -0,0 +1,10 @@
+# Створити список цілих чисел, які вводяться з терміналу та визначити серед них максимальне та мінімальне число.
+
+list_of_numbers=[int(input("Enter a number:")) for i in range(int(input("Enter amount of numbers:")))]
+print("Max number is {}".format(max(list_of_numbers)))
+
+or
+
+list = [1, 89, 80, 65, 17]
+x=max(list)
+print("The maximum number from list is ", x)
