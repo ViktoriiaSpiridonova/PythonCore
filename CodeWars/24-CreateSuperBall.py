c1f4f6f960bf2327765462e8f30b3594e3178d03
Viktoriia <46558739+ViktoriiaSpiridonova@users.noreply.github.com>
2019-02-21 16:13:32 +0200

    Update 24-CreateSuperBall.py

--- CodeWars/24-CreateSuperBall.py
+++ CodeWars/24-CreateSuperBall.py
@@ -3,5 +3,7 @@
 # If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
 
 class Ball(object):
-    def __init__(self, ball_type = "regular"):
-        self.ball_type = ball_type
+    def __init__ (self, ball_type="regular"):
+        self.ball_type="regular"
+        if ball_type=="super":
+            self.ball_type="super"
